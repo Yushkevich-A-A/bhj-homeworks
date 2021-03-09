283944da4fa4f1f5bf1ddc3c7707a5a7172d0a76
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2021-03-09 13:46:25 +0300

    block interests added

---
+++ html-forms/interests/task.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const interestCheck = document.getElementsByClassName('interest__check');
+
+for (let item of interestCheck) {
+    item.addEventListener('change', event => {
+        
+        const blockInternalCheckbox = item.closest('label').nextElementSibling;
+        if (blockInternalCheckbox) {
+            for (let i of blockInternalCheckbox.getElementsByClassName('interest__check')) {
+                i.checked = item.checked;
+            }
+            return;
+        }
+
+        // const blockExternalCheckbox = item.closest('ul.interests').previousElementSibling;
+        // console.log(blockExternalCheckbox);
+        // if (blockExternalCheckbox) {
+        //     blockExternalCheckbox.getElementsByClassName('interest__check')[0].checked = item.checked;
+        //     return;
+        // }
+
+    })
+}
