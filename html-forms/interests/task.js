c677b98aa5edab773d31505270da9d14bf04aae6
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2021-03-10 17:33:05 +0300

    extended interests

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,23 +2,54 @@
 
 const interestCheck = document.getElementsByClassName('interest__check');
 
+
 for (let item of interestCheck) {
     item.addEventListener('change', event => {
-        
-        const blockInternalCheckbox = item.closest('label').nextElementSibling;
-        if (blockInternalCheckbox) {
-            for (let i of blockInternalCheckbox.getElementsByClassName('interest__check')) {
-                i.checked = item.checked;
-            }
-            return;
-        }
-
-        const blockExternalCheckbox = item.closest('ul.interests').previousElementSibling;
-        console.log(blockExternalCheckbox);
-        if (blockExternalCheckbox) {
-            blockExternalCheckbox.getElementsByClassName('interest__check')[0].checked = item.checked;
-            return;
-        }
+        let arr = [];
+        checkingChildElements (item);
+        checkingParentElements (item);
 
     })
+}
+
+function checkingChildElements(item) {
+    let parentRootElement = item.closest('label');
+    if (parentRootElement.nextElementSibling) {
+        let siblingCheckedElements = parentRootElement.nextElementSibling.getElementsByClassName('interest__check');
+        for (let i of siblingCheckedElements) {
+            i.checked = item.checked;
+        }
+    }
+}
+
+function checkingParentElements(item) {
+
+    if (!item.closest('ul.interests')) {
+        return;
+    }
+    let itemInput = item.closest('ul.interests').previousElementSibling.getElementsByClassName('interest__check')[0];
+    let listInterestCheck = item.closest('ul.interests').children;
+
+    for (let i of listInterestCheck) {
+        i = i.getElementsByClassName('interest__check')[0];
+        if (i.checked) {
+            itemInput.checked = i.checked;
+            itemInput.indeterminate = !i.checked;
+            continue;
+        } 
+        itemInput.checked = i.checked;
+        break;
+    }
+
+    if (!itemInput.checked) {
+        for (let i of listInterestCheck) {
+            if (i.checked) {
+                itemInput.indeterminate = false;
+                continue;
+            }
+            itemInput.indeterminate = true;
+        }
+    }
+
+    checkingParentElements(itemInput);
 }
