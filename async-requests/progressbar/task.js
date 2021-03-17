183106896c82d2cb163f3c36e6802f7c940e30d8
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2021-03-17 11:55:52 +0300

    progressbar created

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,17 +2,18 @@
 
 const progress = document.getElementById('progress');
 const form = document.getElementById('form');
-const button = document.getElementById('send');
-const file = document.getElementsByName('file')[0];
 
 form.addEventListener('submit', event => {
-    const formData = new FormData();
-    formData.append('file', file.files[0]);
+    const formData = new FormData(form);
     
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-    // xhr.upload.addEventListener('progress', e => console.log(xhr.upload));
+    xhr.upload.addEventListener('progress', event => {
+        progress.value = (event.loaded / event.total).toFixed(2);
+    })
+
     xhr.send(formData);
     event.preventDefault();
+
 })
 
