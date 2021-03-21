dd7b45e0fae1d507042b3c000436645a2cc422d0
Yushkevich Anton Alekseevich <Mortifer1993@yandex.ru>
2021-03-21 21:52:36 +0300

    popup task created

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -3,13 +3,19 @@
 const subscribeModal = document.getElementById('subscribe-modal');
 const modalClose = subscribeModal.querySelector('.modal__close');
 
-setTimeout(() => subscribeModal.classList.add('modal_active') )
+if(getCookie('name') !== 'true') {
+    setTimeout(() => subscribeModal.classList.add('modal_active') )
+}
 
 modalClose.addEventListener('click', () => {
     subscribeModal.classList.remove('modal_active');
-    let date = new Date().setMinutes(1);
+    let date = new Date().setMinutes(10);
     date = date.toString().split(' ');
-    // document.cookie = 'name=true;';
-    // document.cookie
+    document.cookie = `name=true; Expires=${date[0]}, ${date[2]} ${date[1]} ${date[3]} ${date[4]} ${date[5]}`;
 })
-// Expires=${date[0]}, ${date[2]} ${date[1]} ${date[3]} ${date[4]} ${date[5]}
+
+function getCookie (name) {
+    let value = document.cookie;
+    value = value.split(name + '=');
+    return value.pop().split(';').shift();
+}
